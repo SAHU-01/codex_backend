c6c78b96ff096401fd85380c044623a54ba3f6f1
SAHU-01 <ankita.sahu200102@gmail.com>
2022-05-11 08:24:16 +0530

    deployment fixes !!

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(cors());
 app.use("/members", membersRoute);
 app.use("/events", eventsRoute);
 
-const PORT=process.env.port  || 5050;
+const PORT = process.env.port || 5050;
 
 app.listen(PORT, () => {
   console.log("Backend is running");
